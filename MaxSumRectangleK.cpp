1da53e096e57df00f92beac3ad28f491983e6d7a
Sashank Alladi <sashank6@users.noreply.github.com>
2016-12-16 17:41:21 -0800

    Create MaxSumRectangleK.cpp

---
+++ MaxSumRectangleK.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+	int maxSumSubmatrix(vector<vector<int>>& mat, int k) {
+		int n = mat.size();
+		int m = mat[0].size();
+		for (int i = 1; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[j][i] += mat[j][i - 1];
+			}
+		}
+		int maxsum = INT_MIN;
+		int rowtop = -1, rowbot = -1, colst = -1, colen = -1;
+
+		for (int start = 0; start < m; start++) {
+			for (int end = start; end < m; end++) {
+				int arr[n + 1] = { 0 };
+				for (int row = 0; row < n; row++)
+					arr[row + 1] = mat[row][end]
+							- ((start == 0) ? 0 : mat[row][start - 1]);
+				int gsum = INT_MIN;
+
+				for (int i = 1; i < n + 1; i++)
+					arr[i] += arr[i - 1];
+				for (int i = 0; i < n + 1; i++) {
+					for (int j = 0; j < i; j++) {
+						if ((arr[i] - arr[j]) <= k) {
+							gsum = max(gsum, arr[i] - arr[j]);
+						}
+
+					}
+				}
+				if (gsum > maxsum) {
+					maxsum = gsum;
+				}
+			}
+		}
+		return maxsum;
+
+	}
+};
+
